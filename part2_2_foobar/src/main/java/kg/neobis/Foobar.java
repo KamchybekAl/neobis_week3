bf3c672366fc4b3ddded0cb3f9ac86defcb8588e
Alzhanbaev Kamchybek <aljanbaevkam@gmail.com>
2023-08-07 01:04:37 +0600

    Added task week3 part2_2

---
+++ part2_2_foobar/src/main/java/kg/neobis/Foobar.java
@@ -0,0 +1,11 @@
+package kg.neobis;
+
+public class Foobar {
+    public void foobar(int num) {
+        if (num % 2 == 0) {
+            System.out.println("foo " + num);
+        } else {
+            System.out.println("bar " + num);
+        }
+    }
+}
