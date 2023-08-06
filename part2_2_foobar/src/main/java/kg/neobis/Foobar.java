97b386c152e5978247f9d85faef42769953a54c8
Alzhanbaev Kamchybek <aljanbaevkam@gmail.com>
2023-08-07 01:22:17 +0600

    Completed task week3 part2_2

--- part2_2_foobar/src/main/java/kg/neobis/Foobar.java
+++ part2_2_foobar/src/main/java/kg/neobis/Foobar.java
@@ -4,6 +4,7 @@ public class Foobar {
     public void foobar(int num) {
         if (num % 2 == 0) {
             System.out.println("foo " + num);
+
         } else {
             System.out.println("bar " + num);
         }
--- part2_2_foobar/src/main/java/kg/neobis/Main.java
+++ part2_2_foobar/src/main/java/kg/neobis/Main.java
@@ -2,8 +2,6 @@ package kg.neobis;
 
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Foobar foobar = new Foobar();
